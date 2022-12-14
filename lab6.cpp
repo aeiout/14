2840496979a392a24ca704e1401a547fc7b153be
ueaarthorn <yeaarthon13117@gmail.com>
2022-12-14 17:09:59 +0700

    problem2.3

--- lab6.cpp
+++ lab6.cpp
@@ -1,5 +1,18 @@
 #include<iostream>
 using namespace std;
+int findDivisor(int x){
+    int i=2;
+    while(i<=x){
+    if(x%i==0){
+        return i;
+    }else{
+        i++;
+    }
+}
+}
 int main(){
-    cout<<"x";
+    int x;
+    cout<<"Input :";
+    cin >> x;
+    cout<<"Output :"<<findDivisor(x);
 }
